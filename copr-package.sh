63ebcf34384007c3e50ac90e544370664d07c0cb
Juan Ruben Nolasco Silva <juan.ruben.nolasco.silva@gmail.com>
2025-09-25 10:17:40 -0600

    - Deprecating some files, and removing unnecessary applications from tasks.

--- copr-package.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Install Preload:
-sudo dnf copr enable elxreno/preload -y && sudo dnf install preload -y
-
-# Better Fonts:
-sudo dnf copr enable dawid/better_fonts -y
-sudo dnf install fontconfig-font-replacements -y
-sudo dnf install fontconfig-enhanced-defaults -y
